5010c5220a7567b62a67a6c00748dd6086f3faab
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-29 12:12:52 -0600

    :sparkles: Finish drf api

--- django-rest-framework/01-crud-django-react/backend/api/urls.py
+++ django-rest-framework/01-crud-django-react/backend/api/urls.py
@@ -1,4 +1,10 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
+  path('get/', views.get_blogs),
+  path('get-one/<int:pk>/', views.get_blog),
+  path('post/', views.post_blog),
+  path('put/<int:pk>/', views.put_blog),
+  path('delete/<int:pk>/', views.delete_blog),
 ]
--- django-rest-framework/01-crud-django-react/backend/api/views.py
+++ django-rest-framework/01-crud-django-react/backend/api/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+from .models import Blog
+from .serializers import BlogSerializer
+
+# Creamos un método GET de API
+@api_view(['GET'])
+def get_blogs(request):
+    blog = Blog.objects.all()
+    serializer = BlogSerializer(blog, many=True)
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def get_blog(request, pk):
+    blog = Blog.objects.get(id=pk)
+    serializer = BlogSerializer(blog, many=False)
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def post_blog(request):
+    data = request.data
+    blog = Blog.objects.create(
+        body=data['body'],
+    )
+    serializer = BlogSerializer(blog, many=False)
+
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def put_blog(request, pk):
+    data = request.data
+    blog = Blog.objects.get(id=pk)
+    serializer = BlogSerializer(blog, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+    return Response(serializer.errors)
+
+@api_view(['DELETE'])
+def delete_blog(request, pk):
+    blog = Blog.objects.get(id=pk)
+    blog.delete()
+
+    return Response('Blog was deleted!')
