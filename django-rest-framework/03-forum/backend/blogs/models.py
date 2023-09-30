8a52be9896ff3f843a79c048b3c1f0332dd64eac
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-29 22:00:00 -0600

    :sparkles: Add models for blogs

---
+++ django-rest-framework/03-forum/backend/blogs/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from users.models import User
+
+class Blog(models.Model):
+    body = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    text = models.CharField(max_length=150)
+    date = models.DateTimeField(auto_now_add=True)
