d5d814c6460b2b32a2cf0bd6fd3b0d4beec43ec6
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-30 20:52:26 -0600

    :sparkles: Finish user endpoints

--- django-rest-framework/03-forum/backend/users/urls.py
+++ django-rest-framework/03-forum/backend/users/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from . import views
 
 urlpatterns = [
     path('register/', views.register),
     path('login/', views.MyTokenObtainPairView.as_view()),
     path('token/', TokenObtainPairView.as_view()),
     path('token/refresh/', TokenRefreshView.as_view()),
+    path('update-user/', views.update_user),
+    path('upload-image/', views.upload_image),
+    path('user-profile/', views.get_user_profile),
+    path('user-info/<int:pk>/', views.get_user),
+    path('users-list/', views.get_users),
 ]
--- django-rest-framework/03-forum/backend/users/views.py
+++ django-rest-framework/03-forum/backend/users/views.py
@@ -1,9 +1,8 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-from django.contrib.auth.hashers import make_password  
-from rest_framework import status
+from django.contrib.auth.hashers import make_password
 
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -41,3 +40,53 @@ def register(request):
     except:
         message = {'detail': 'Error registering the user!'}
         return Response(message, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_user(request):
+    user = request.user
+    user_serializer = UserSerializerWithToken(user, many=False)
+
+    data = request.data
+
+    user.user_name = data['user_name']
+    user.bio = data['bio']
+    user.email = data['email']
+    if data['password'] != '':
+        user.password = make_password(data['password'])
+
+    user.save()
+
+    return Response(user_serializer.data)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def upload_image(request):
+    data = request.data
+    user_id = data['user_id']
+    user = User.objects.get(id=user_id)
+    user.image = request.FILES.get('image')
+    user.save()
+
+    return Response('Image was uploaded')
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user_profile(request):
+    user = request.user
+    user_serializer = UserSerializer(user, many=False)
+    return Response(user_serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_user(request, pk):
+    user = User.objects.get(id=pk)
+    user_serializer = UserSerializer(user, many=False)
+    return Response(user_serializer.data)
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_users(request):
+    users = User.objects.all()
+    users_serializer = UserSerializer(users, many=True)
+    return Response(users_serializer.data)
